b4975390dd147fadad387fb595fc7249da124635
nailainggar <nailainggar25@gmail.com>
2021-02-27 01:45:13 -0800

    naila

--- database/seeders/ProvinsiSeeder.php
+++ database/seeders/ProvinsiSeeder.php
@@ -7,11 +7,7 @@ use Illuminate\Support\Facades\DB;
 
 class ProvinsiSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+   
     public function run()
     {
         $provinsi = [
